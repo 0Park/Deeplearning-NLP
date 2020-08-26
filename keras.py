a7ba8949d21a5d0c68264f7795aa5531029ee53a
Park young hun <dudgns0808@gmail.com>
2020-08-26 15:06:33 +0900

    '0826'

--- keras.py
+++ keras.py
@@ -103,11 +103,14 @@ plt.show()
 
 # epoch 4번이후 overfitting 문제 포착
 #%% model 재구성
+from keras import regularizers
 model=models.Sequential()
-model.add(layers.Dense(16,activation='relu',input_shape=(10000,)))
-model.add(layers.Dense(16,activation='relu'))
+model.add(layers.Dense(16,kernel_regularizer=regularizers.l2(0.001),activation='relu',input_shape=(10000,)))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(16,kernel_regularizer=regularizers.l2(0.001),activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(1,activation='sigmoid'))
-
+#%%
 model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
 model.fit(X_train,y_train,epochs=4,batch_size=512)
 results=model.evaluate(X_test,y_test)
@@ -244,3 +247,112 @@ print(results)
 #%% 보스턴 주택 데이터
 from keras.datasets import boston_housing
 (train_data,train_targets),(test_data,test_targets)=boston_housing.load_data()
+#%% 데이터 정규화하기
+mean=train_data.mean(axis=0)
+train_data-=mean
+std=train_data.std(axis=0)
+train_data/=std
+
+test_data-=mean
+test_data/=std
+#%% 모델 정의하기
+from keras import models
+from keras import layers
+
+def build_models():
+    
+    model=models.Sequential()
+    model.add(layers.Dense(64,activation='relu',input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64,activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop',loss='mse',metrics=['mae'])
+    
+    return model
+#%% k겹 검증하기
+import numpy as np
+k=4
+num_val_samples=len(train_data)//k
+num_epochs=100
+all_scores=[]
+for i in range(k):
+    print('처리중인 폴드#',i)
+    val_data=train_data[i*num_val_samples:(i+1)*num_val_samples]
+    val_targets=train_targets[i*num_val_samples:(i+1)*num_val_samples]
+    
+    partial_train_data=np.concatenate([train_data[:i*num_val_samples],train_data[(i+1)*num_val_samples:]],axis=0)
+    partial_train_targets=np.concatenate([train_targets[:i*num_val_samples],train_targets[(i+1)*num_val_samples:]],axis=0)
+    
+    model=build_models()
+    model.fit(partial_train_data,partial_train_targets,epochs=num_epochs,batch_size=1,verbose=0)
+    val_mse,val_mae=model.evaluate(val_data,val_targets,verbose=0)
+    all_scores.append(val_mae)
+    
+#%%
+print(all_scores)
+print(np.mean(all_scores))
+#%% 각 폴드에서 검증 점수를 로그에 저장하기
+num_epochs=500
+all_mae_histories=[]
+
+for i in range(k):
+    print('처리중인 폴드#',i)
+    val_data=train_data[i*num_val_samples:(i+1)*num_val_samples]
+    val_targets=train_targets[i*num_val_samples:(i+1)*num_val_samples]
+    
+    partial_train_data=np.concatenate([train_data[:i*num_val_samples],train_data[(i+1)*num_val_samples:]],axis=0)
+    partial_train_targets=np.concatenate([train_targets[:i*num_val_samples],train_targets[(i+1)*num_val_samples:]],axis=0)
+    
+    model=build_models()
+    history=model.fit(partial_train_data,partial_train_targets,validation_data=(val_data,val_targets),epochs=num_epochs,batch_size=1,verbose=0)
+    mae_history=history.history['val_mae']
+    all_mae_histories.append(mae_history)
+
+#%% k겹 검증 점수 평균을 기록하기
+average_mae_history=[np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+#%% 검증 점수 그래프그리기
+import matplotlib.pyplot as plt
+
+plt.plot(range(1,len(average_mae_history)+1),average_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+#%% 처음 10개의 데이터 포인트를 제외한 검증 점수 그리기
+
+def smooth_curve(points,factor=0.9):
+    smoothed_points=[]
+    
+    for point in points:
+        if smoothed_points:
+            previous=smoothed_points[-1]
+            smoothed_points.append(previous*factor+point*(1-factor))
+        else:
+            smoothed_points.append(point)
+    
+    return smoothed_points
+
+smooth_mae_history=smooth_curve(average_mae_history[10:])
+
+plt.plot(range(1,len(smooth_mae_history)+1),smooth_mae_history)
+plt.xlabel('Epochs')
+
+#%% 최종 모델 훈련하기
+model=build_models()
+model.fit(train_data,train_targets,epochs=80,batch_size=16,verbose=0)
+test_mse_score, test_mae_score=model.evaluate(test_data,test_targets)
+#%%
+print(test_mae_score)
+
+#%% hold out 검증
+num_validation_samples=101
+np.random.shuffle(train_data)
+validation_data=train_data[:num_validation_samples]
+data=train_data[num_validation_samples:]
+
+model=build_models()
+model.train(train_data)
+validation_score=model.evaluate(validation_data)
+
+model=build_models()
+model.train(np.concatenate([train_data,validation_data]))
+test_score=model.evaluate(test_data)
