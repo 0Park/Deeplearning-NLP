cc003baecd61e403c68c41066fd55bd622f5875e
Park young hun <dudgns0808@gmail.com>
2020-09-17 00:33:26 +0900

    'FINAL'

--- making_rnn.py
+++ making_rnn.py
@@ -4,8 +4,9 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def loss_function(y_target,y,batch_size):
-    loss=-np.sum(((y_target*np.log(y+0.0001))+(1-y_target*np.log(1-y+0.0001))),axis=0)
+    loss=abs(-np.sum(((y_target*np.log(y+0.1))+(1-y_target*np.log(1-y+0.1))),axis=1))
     loss/=batch_size
+    
     return loss
 
 class RNN_layer:
@@ -21,7 +22,7 @@ class RNN_layer:
         self.b=np.random.random((hidden_size,1))
         self.h_times=np.random.random((time_steps,hidden_size,batch_size))
         self.W_y=np.random.uniform(-np.sqrt(1./hidden_size),np.sqrt(1./hidden_size),(1,hidden_size))
-        self.b_y=np.zeros((1,1))
+        self.b_y=np.random.random((1,1))
     
     def forward_RNN(self,x_data):
         # batch_size,time_stpes,input_dim
@@ -31,19 +32,17 @@ class RNN_layer:
             data[i]=temp.T
         data=data.T
         '''
-        for i in range(1,self.time_steps+1):
-            if i==1:
+        for i in range(self.time_steps):
+            if i==0:
                 #batch time input
-                self.h_times[i-1]=np.tanh(np.dot(self.W_x,x_data[i-1])+self.b)
+                self.h_times[i]=np.tanh(np.dot(self.W_x,x_data[i])+self.b)
             else:
-                self.h_times[i-1]=np.tanh(np.dot(self.W_x,x_data[i-1])+np.dot(self.W_h,self.h_times[i-2])+self.b)
+                self.h_times[i]=np.tanh(np.dot(self.W_x,x_data[i])+np.dot(self.W_h,self.h_times[i-1])+self.b)
         a=np.dot(self.W_y,self.h_times[self.time_steps-1])+self.b_y
-        y=sigmoid(a).T
+        y=sigmoid(a)
         return y
     
     def backward_RNN(self,y_target,y,x_data):
-        target=y_target.T
-        y_temp=y.T
         '''
         data=np.zeros((self.batch_size,self.input_dim,self.time_steps))
         for i,temp in enumerate(x_data):
@@ -56,7 +55,7 @@ class RNN_layer:
         db=np.zeros_like(self.b)
         db_y=np.zeros_like(self.b_y)
         dh=np.zeros_like(self.h_times)
-        da=-(np.divide(target,y_temp+0.00001)-np.divide(1-target,1-y_temp+0.00001))*y_temp*(1-y_temp)
+        da=y-y_target
         
         # y shape=(1,batch_size4)
         n=self.time_steps
@@ -64,15 +63,16 @@ class RNN_layer:
             if i==1:
                 dh[n-i]=np.dot(self.W_y.T,da)
             else:
-                dh[n-i]=np.dot(self.W_h.T,(1-np.exp2(self.h_times[n-i+1]))*dh[n-i+1])
+                dh[n-i]=np.dot(self.W_h.T,(1-np.square(self.h_times[n-i+1]))*dh[n-i+1])
         
-        for i in range(1,n+1):
+        for i in range(n):
             db_y+=np.sum(da,axis=1).T/self.batch_size
-            a=np.sum((1-np.exp2(self.h_times[n-i]))*dh[i-1],axis=1)/self.batch_size
+            a=np.sum((1-np.square(self.h_times[i]))*dh[i],axis=1)/self.batch_size
             db+=(db.T+a).T
-            dW_y+=np.dot(da,self.h_times[n-i].T)
-            dW_h+=np.dot((1-np.exp2(self.h_times[n-i]))*dh[n-i],self.h_times[n-i-1].T)
-            dW_x+=np.dot((1-np.exp2(self.h_times[n-i]))*dh[n-i],x_data[n-i].T)
+            dW_y+=np.dot(da,self.h_times[i].T)
+            if i!=0:           
+                dW_h+=np.dot((1-np.square(self.h_times[i]))*dh[i],self.h_times[i-1].T)
+            dW_x+=np.dot((1-np.square(self.h_times[i]))*dh[i],x_data[i].T)
             
         return dW_h,dW_x,dW_y,db,db_y
     
@@ -86,15 +86,16 @@ class RNN_layer:
         
 #%%
 
-hidden_size=6
+hidden_size=7
 time_steps=6
 input_dim=12
-data_size=1000       
+data_size=10000
 
 x_data=np.random.randint(0,2,size=(time_steps,input_dim,data_size))
 #x_data=np.random.randint(0,2,size=(data_size,time_steps,input_dim))
-y_train=np.random.randint(0,2,size=(data_size,1))
+y_train=np.random.randint(0,2,size=(1,data_size))
 #%%
+import matplotlib.pyplot as plt
 
 r=RNN_layer(hidden_size, time_steps, input_dim,data_size)
 
@@ -104,7 +105,10 @@ for i in range(epochs):
     y=r.forward_RNN(x_data)
     loss[i]=loss_function(y_train, y, data_size)
     print(i)
-    r.update_model(0.01, y_train, y, x_data)
+    r.update_model(0.0001, y_train, y, x_data)  # learning rate 조절
+
+plt.plot(range(1,31),loss)
+plt.show()
 #%%
 a=[[[1,2,3]],[[7,8,9]]]
 a=np.array(a)
