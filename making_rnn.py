458a804cc6684a618e7a5fea13a9629f84030b2f
Park young hun <dudgns0808@gmail.com>
2020-09-16 10:55:44 +0900

    'edit'

--- making_rnn.py
+++ making_rnn.py
@@ -4,7 +4,7 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def loss_function(y_target,y,batch_size):
-    loss=-np.sum(((y_target*np.log(y))+(1-y_target*np.log(1-y))),axis=0)
+    loss=-np.sum(((y_target*np.log(y+0.0001))+(1-y_target*np.log(1-y+0.0001))),axis=0)
     loss/=batch_size
     return loss
 
@@ -16,15 +16,21 @@ class RNN_layer:
         self.input_dim=input_dim
         self.batch_size=batch_size
         
-        self.W_h=np.random.random((hidden_size,hidden_size))
-        self.W_x=np.random.random((hidden_size,input_dim))
+        self.W_h=np.random.uniform(-np.sqrt(1./hidden_size),np.sqrt(1./hidden_size),(hidden_size,hidden_size))
+        self.W_x=np.random.uniform(-np.sqrt(1./input_dim),np.sqrt(1./input_dim),(hidden_size,input_dim))
         self.b=np.random.random((hidden_size,1))
         self.h_times=np.random.random((time_steps,hidden_size,batch_size))
-        self.W_y=np.random.random((1,hidden_size))
+        self.W_y=np.random.uniform(-np.sqrt(1./hidden_size),np.sqrt(1./hidden_size),(1,hidden_size))
         self.b_y=np.zeros((1,1))
     
     def forward_RNN(self,x_data):
-
+        # batch_size,time_stpes,input_dim
+        '''
+        data=np.zeros((self.batch_size,self.input_dim,self.time_steps))
+        for i,temp in enumerate(x_data):
+            data[i]=temp.T
+        data=data.T
+        '''
         for i in range(1,self.time_steps+1):
             if i==1:
                 #batch time input
@@ -38,14 +44,19 @@ class RNN_layer:
     def backward_RNN(self,y_target,y,x_data):
         target=y_target.T
         y_temp=y.T
-        
+        '''
+        data=np.zeros((self.batch_size,self.input_dim,self.time_steps))
+        for i,temp in enumerate(x_data):
+            data[i]=temp.T
+        data=data.T
+        '''
         dW_h=np.zeros_like(self.W_h)
         dW_x=np.zeros_like(self.W_x)
         dW_y=np.zeros_like(self.W_y)
         db=np.zeros_like(self.b)
         db_y=np.zeros_like(self.b_y)
         dh=np.zeros_like(self.h_times)
-        da=-(np.divide(target,y_temp)-np.divide(1-target,1-y_temp))*y_temp*(1-y_temp)
+        da=-(np.divide(target,y_temp+0.00001)-np.divide(1-target,1-y_temp+0.00001))*y_temp*(1-y_temp)
         
         # y shape=(1,batch_size4)
         n=self.time_steps
@@ -53,7 +64,7 @@ class RNN_layer:
             if i==1:
                 dh[n-i]=np.dot(self.W_y.T,da)
             else:
-                dh[n-i]=np.dot(self.W_h.T,(1-np.exp2(self.h_times[n-i+1])*dh[n-i+1]))
+                dh[n-i]=np.dot(self.W_h.T,(1-np.exp2(self.h_times[n-i+1]))*dh[n-i+1])
         
         for i in range(1,n+1):
             db_y+=np.sum(da,axis=1).T/self.batch_size
@@ -61,7 +72,7 @@ class RNN_layer:
             db+=(db.T+a).T
             dW_y+=np.dot(da,self.h_times[n-i].T)
             dW_h+=np.dot((1-np.exp2(self.h_times[n-i]))*dh[n-i],self.h_times[n-i-1].T)
-            dW_x+=np.dot(1-np.exp2(self.h_times[n-i])*dh[n-i],x_data[n-i].T)
+            dW_x+=np.dot((1-np.exp2(self.h_times[n-i]))*dh[n-i],x_data[n-i].T)
             
         return dW_h,dW_x,dW_y,db,db_y
     
@@ -74,27 +85,30 @@ class RNN_layer:
         self.b_y=self.b_y-learning_rate*derivatives[4]
         
 #%%
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences  
-from tensorflow.keras.utils import to_categorical  
-hidden_size=3
-time_steps=2
-input_dim=4
-data_size=7        
+
+hidden_size=6
+time_steps=6
+input_dim=12
+data_size=1000       
 
 x_data=np.random.randint(0,2,size=(time_steps,input_dim,data_size))
+#x_data=np.random.randint(0,2,size=(data_size,time_steps,input_dim))
 y_train=np.random.randint(0,2,size=(data_size,1))
 #%%
 
 r=RNN_layer(hidden_size, time_steps, input_dim,data_size)
 
-epochs=100
+epochs=30
 loss=np.zeros((epochs,1))
 for i in range(epochs):   
     y=r.forward_RNN(x_data)
     loss[i]=loss_function(y_train, y, data_size)
     print(i)
     r.update_model(0.01, y_train, y, x_data)
-    
+#%%
+a=[[[1,2,3]],[[7,8,9]]]
+a=np.array(a)
+print(a.shape)
+print(a.T.shape)   
 
     
